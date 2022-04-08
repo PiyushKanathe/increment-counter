5fd28b810b51dd641617be88fac9c3a48ed89647
Piyush Kanathe <dit.piyush@gmail.com>
2022-04-08 21:20:39 +0530

    increment counter code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import countContext from "./context";
+import React, { useState } from "react";
+import ButtonComp from './ButtonComp';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <countContext.Provider value={{count, handleCount}}>
+        <ButtonComp></ButtonComp>
+      </countContext.Provider>
       </header>
     </div>
   );
