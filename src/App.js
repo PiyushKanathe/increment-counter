b99041bc5e0ef9665c1a304b1df05993059329e2
Piyush Kanathe <dit.piyush@gmail.com>
2024-02-14 10:55:37 +0530

    new dev

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import "./App.css";
 import countContext from "./context";
-import React, { useState } from "react";
-import ButtonComp from './ButtonComp';
+import React, { useCallback, useState } from "react";
+import ButtonComp from "./ButtonComp";
 
 function App() {
   const [count, setCount] = useState(0);
-  const handleCount = () => {
-    setCount(count + 1);
-  };
+
+  const incrementCount = useCallback(() => {
+    setCount((pre) => pre + 1);
+  }, []);
+
+  const decrementCount = useCallback(() => {
+    setCount((pre) => {
+      if (pre > 0) {
+        return pre - 1;
+      }
+      return pre
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      <countContext.Provider value={{count, handleCount}}>
-        <ButtonComp></ButtonComp>
-      </countContext.Provider>
+        <countContext.Provider
+          value={{ incrementCount, decrementCount }}
+        >
+          <div>
+            <span>{count}</span>
+          </div>
+          <div className="button-container">
+            <ButtonComp>+</ButtonComp>
+            <ButtonComp>-</ButtonComp>
+          </div>
+        </countContext.Provider>
       </header>
     </div>
   );
--- src/ButtonComp.js
+++ src/ButtonComp.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import countContext from "./context";
 
-const ButtonComp = () => {
+const ButtonComp = (props) => {
     const context = useContext(countContext)
-    const { handleCount, count } = context
+    const { decrementCount, incrementCount } = context
+    let buttonLabel = props.children === "+"? "Increment": "Decrement"
     return(
-        <button onClick={handleCount}>Increment: {count}</button>
+        <button onClick={props.children === "+"? incrementCount :decrementCount}>{buttonLabel}</button>
     )
 }
 
